02982038db874171ba7ab1600f724f3a1d8c5b6d
olyaaanemm <90577981+olyaaanemm@users.noreply.github.com>
2022-01-07 00:12:34 +0300

    Update Program.cs

--- Simple.ThreadPool/Program.cs
+++ Simple.ThreadPool/Program.cs
@@ -5,28 +5,18 @@ namespace SimpleThreadPool
 {
     public static class Program
     {
-        static int  shared_counter = 0;
+        static volatile int   shared_counter = 0;
         static void Main()
         {
             SimpleThreadPool pool = new SimpleThreadPool();
             
-            var random = new Random();
-            Action<int> randomizer = (index =>
-            {
-                Console.WriteLine("{0}: Working on index {1}", Thread.CurrentThread.Name, index);
-                Thread.Sleep(random.Next(20, 400));
-                Console.WriteLine("{0}: Ending {1}", Thread.CurrentThread.Name, index);
-            });
-
-            
             for (int i = 0; i < 100500; ++i)
             {
                 var i1 = i;
-                pool.Submit( () => ++shared_counter);
+                pool.Submit( () => Interlocked.Increment(ref shared_counter));
             }
             
             pool.Join();
-            Thread.Sleep(5);
             Console.WriteLine(shared_counter);
         }
     }
