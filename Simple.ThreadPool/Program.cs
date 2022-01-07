fbf8bc49214832feac2436fa75e9e68a4bf8342e
olyaaanemm <90577981+olyaaanemm@users.noreply.github.com>
2022-01-07 22:12:25 +0300

    Update Program.cs

--- Simple.ThreadPool/Program.cs
+++ Simple.ThreadPool/Program.cs
@@ -13,9 +13,11 @@ namespace SimpleThreadPool
             for (int i = 0; i < 100500; ++i)
             {
                 var i1 = i;
-                pool.Submit( () => Interlocked.Increment(ref shared_counter));
+                pool.Submit( () =>
+                {
+                    Interlocked.Increment(ref shared_counter);
+                });
             }
-            
             pool.Join();
             Console.WriteLine(shared_counter);
         }
